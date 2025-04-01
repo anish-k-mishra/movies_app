bcb706b353a60b5a031c54976ef95f2eff6c2d7f
Anish Mishra <anishkumarc92@gmail.com>
2025-04-01 12:01:46 +0530

    Implemented Search functionality and search page

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,96 +1,122 @@
-import {View, Text, Image, FlatList, ActivityIndicator} from 'react-native';
-import React, {useState, useEffect} from 'react';
-import {images} from "@/constants/images";
-import MovieCard from "@/components/MovieCard";
+import { useState, useEffect } from "react";
+import { View, Text, ActivityIndicator, FlatList, Image } from "react-native";
+
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
+
 import useFetch from "@/services/useFetch";
-import {fetchMovies} from "@/services/api";
-import {icons} from "@/constants/icons";
+import { fetchMovies } from "@/services/api";
+import { updateSearchCount } from "@/services/appwrite";
+
 import SearchBar from "@/components/SearchBar";
+import MovieDisplayCard from "@/components/MovieCard";
 
 const Search = () => {
     const [searchQuery, setSearchQuery] = useState("");
 
     const {
-        data: movies,
+        data: movies = [],
         loading,
         error,
         refetch: loadMovies,
         reset,
-    } = useFetch(() => fetchMovies({query: searchQuery
-    }), false);
+    } = useFetch(() => fetchMovies({ query: searchQuery }), false);
 
+    const handleSearch = (text: string) => {
+        setSearchQuery(text);
+    };
+
+    // Debounced search effect
     useEffect(() => {
-        const timeOutId = setTimeout( async () => {
+        const timeoutId = setTimeout(async () => {
             if (searchQuery.trim()) {
                 await loadMovies();
+
+                // Call updateSearchCount only if there are results
+                if (movies?.length! > 0 && movies?.[0]) {
+                    await updateSearchCount(searchQuery, movies[0]);
+                }
             } else {
-                reset()
+                reset();
             }
         }, 500);
 
-        return () => clearTimeout(timeOutId);
+        return () => clearTimeout(timeoutId);
     }, [searchQuery]);
+
     return (
         <View className="flex-1 bg-primary">
-            <Image source = {images.bg} className="flex-1 absolute w-full z-0" resizeMode="cover" />
+            <Image
+                source={images.bg}
+                className="flex-1 absolute w-full z-0"
+                resizeMode="cover"
+            />
 
             <FlatList
-                data = {movies}
-                renderItem={({item}) => <MovieCard {...item}/>}
-                keyExtractor={(item) => item.id.toString()}
                 className="px-5"
+                data={movies as Movie[]}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => <MovieDisplayCard {...item} />}
                 numColumns={3}
                 columnWrapperStyle={{
-                    justifyContent: 'center',
-                    gap:16,
+                    justifyContent: "flex-start",
+                    gap: 16,
                     marginVertical: 16,
                 }}
-                contentContainerStyle={{paddingTop: 100}}
+                contentContainerStyle={{ paddingBottom: 100 }}
                 ListHeaderComponent={
                     <>
                         <View className="w-full flex-row justify-center mt-20 items-center">
-                            <Image source = {icons.logo} className="w-12 h-10"/>
+                            <Image source={icons.logo} className="w-12 h-10" />
                         </View>
 
                         <View className="my-5">
                             <SearchBar
-                                placeholder='Search your favourite movie...'
-                                value = {searchQuery}
-                                onChangeText={(text:string) => setSearchQuery(text)}
+                                placeholder="Search for a movie"
+                                value={searchQuery}
+                                onChangeText={handleSearch}
                             />
                         </View>
 
                         {loading && (
-                            <ActivityIndicator size = "large" color = "#0000ff" className="my-3" />
+                            <ActivityIndicator
+                                size="large"
+                                color="#0000ff"
+                                className="my-3"
+                            />
                         )}
 
                         {error && (
-                            <Text className="Text-red-500 px-5 my-3">
+                            <Text className="text-red-500 px-5 my-3">
                                 Error: {error.message}
                             </Text>
                         )}
 
-                        {!loading && !error && searchQuery.trim() && movies?.length > 0 && (
-                            <Text className="text-xl text-white font-bold">
-                                Search Results for {' '}
-                                <Text className="text-accent">{searchQuery}</Text>
-                            </Text>
-                        )}
+                        {!loading &&
+                            !error &&
+                            searchQuery.trim() &&
+                            movies?.length! > 0 && (
+                                <Text className="text-xl text-white font-bold">
+                                    Search Results for{" "}
+                                    <Text className="text-accent">{searchQuery}</Text>
+                                </Text>
+                            )}
                     </>
                 }
-
                 ListEmptyComponent={
                     !loading && !error ? (
-                        <View className="mt-10px-5">
+                        <View className="mt-10 px-5">
                             <Text className="text-center text-gray-500">
-                                {searchQuery.trim() ? 'No movies found!' : 'Search for a movie...'}
+                                {searchQuery.trim()
+                                    ? "No movies found"
+                                    : "Start typing to search for movies"}
                             </Text>
                         </View>
-                    ): null
+                    ) : null
                 }
             />
         </View>
-    )
-}
+    );
+};
 
 export default Search;
