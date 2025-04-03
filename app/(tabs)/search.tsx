a058468cc91012992522585709390582b9778515
Anish Mishra <anishkumarc92@gmail.com>
2025-04-03 12:05:54 +0530

    Trending appwrite fixed, and trending card started

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -32,10 +32,7 @@ const Search = () => {
             if (searchQuery.trim()) {
                 await loadMovies();
 
-                // Call updateSearchCount only if there are results
-                if (movies?.length! > 0 && movies?.[0]) {
-                    await updateSearchCount(searchQuery, movies[0]);
-                }
+
             } else {
                 reset();
             }
@@ -43,6 +40,13 @@ const Search = () => {
 
         return () => clearTimeout(timeoutId);
     }, [searchQuery]);
+
+    useEffect(() => {
+        // Call updateSearchCount only if there are results
+        if (movies?.length! > 0 && movies?.[0]) {
+            updateSearchCount(searchQuery, movies[0]);
+        }
+    }, [movies]);
 
     return (
         <View className="flex-1 bg-primary">
